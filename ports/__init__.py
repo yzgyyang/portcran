ecc2f8d34fca6cf17a43b546c3b9bac286c237b2
David Naylor <naylor.b.david@gmail.com>
2016-12-19 15:35:04 +0200

    Import uses to register classes.

--- ports/__init__.py
+++ ports/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
 from ports.core import Dependency, Platform, Port, PortError, PortStub, Ports, Uses  # pylint: disable=import-self
+from ports import uses
 
 __all__ = ["Dependency", "Platform", "Port", "PortError", "PortStub", "Ports", "Uses"]
