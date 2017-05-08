0e1eed9e8d95808f7521d9671f5f7034d78d76af
David Naylor <naylor.b.david@gmail.com>
2017-05-08 21:27:13 +0200

    test: improve mypy by using incremental model

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pylint -d missing-docstring,locally-disabled,import-error ports portcran.py
-mypy --py2 --strict --ignore-missing-imports ports portcran.py
+mypy -i --py2 --strict --ignore-missing-imports ports portcran.py
