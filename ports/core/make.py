06d9f603b0a1795db795b2db95cb84e33a236aaa
David Naylor <naylor.b.david@gmail.com>
2018-12-24 09:09:16 +0200

    make: convert to pathlib.Path and add documentation

--- ports/core/make.py
+++ ports/core/make.py
@@ -1,7 +1,8 @@
+"""Simple representation of a bmake(1) Makefile."""
 from collections import OrderedDict
+from pathlib import Path
 from re import compile as re_compile
 from typing import Dict, Iterable, List, Optional, Set, Union
-from plumbum.path import LocalPath
 from ..utilities import Stream
 
 __all__ = ["MakeDict", "make_var", "make_vars"]
@@ -9,13 +10,15 @@ __all__ = ["MakeDict", "make_var", "make_vars"]
 VARIABLE_ASSIGNMENT = re_compile(r"^\s*(\w+)\s*([+?:]?)=(.*)$")
 
 
-def make_var(portdir: LocalPath, var: str) -> List[str]:
-    return make_vars(portdir)[var]
+def make_var(path: Path, var: str) -> List[str]:
+    """Return a specified variable from the Makefile in the specified path."""
+    return make_vars(path)[var]
 
 
-def make_vars(portdir: LocalPath) -> "MakeDict":
+def make_vars(path: Path) -> "MakeDict":
+    """Return an object representing the variables from the Makefile in the specified path."""
     variables = MakeDict()
-    with open(portdir / "Makefile", "r") as makefile:
+    with open(path / "Makefile", "r") as makefile:
         data = Stream(makefile.readlines(), lambda x: x.split("#", 2)[0].rstrip())
         while True:
             lines = list(data.take_while(lambda x: x.endswith("\\"), inclusive=True))
@@ -39,15 +42,31 @@ def make_vars(portdir: LocalPath) -> "MakeDict":
     return variables
 
 
-class MakeDict(object):
+class MakeDict:
+    """
+    A representation of a bmake(1) Makefile.
+
+    This class only handles a simplified subset of the bmake(1) Makefile.  Only variable assingment and simple
+    variable expansion is supported.
+
+    The following Makefile variable assignment operators map to this class as follows:
+     - "=" -> MakeDict.set()
+     - "+=" -> MakeDict.extend()
+     - "?=" -> MakeDict.add()
+     - ":=" -> not directly supported (but MakeDict.set(MakeDict[n])) could approximate)
+    """
+
     def __init__(self) -> None:
+        """Initialise a new instance of the MakeDict class."""
         self._variables: Dict[str, List[str]] = OrderedDict()
         self._internal: Set[str] = set()
 
     def __contains__(self, item: str) -> bool:
+        """Indicate if the specified variable name is contained in this collection."""
         return item in self._variables
 
     def __getitem__(self, item: str) -> List[str]:
+        """Get a variable's value, expanding if needed."""
         values = self._variables[item]
         subbed_values: List[str] = []
         for value in values:
@@ -63,6 +82,7 @@ class MakeDict(object):
         return subbed_values
 
     def __str__(self) -> str:
+        """Return a string representation of all public variables."""
         unpopped = []
         for key, value in self._variables.items():
             if key not in self._internal:
@@ -70,24 +90,34 @@ class MakeDict(object):
         return ", ".join(unpopped)
 
     def add(self, name: str, values: List[str]) -> None:
+        """Add (if not existing) the specified variable name and list of string values to this collection."""
         if name not in self._variables:
             self.set(name, values)
 
     @property
     def all_popped(self) -> bool:
+        """Indicate if all variables have been popped."""
         return len(self._internal) == len(self._variables)
 
     @property
     def variables(self) -> Iterable[str]:
+        """List of public variable names in this collection."""
         return [var for var in self._variables.keys() if var not in self._internal]
 
     def extend(self, name: str, values: List[str]) -> None:
+        """Extend the specified variable with the specified list of strings."""
         if name in self._variables:
             self._variables[name].extend(values)
         else:
             self.set(name, values)
 
     def pop(self, name: str, **kwargs: List[str]) -> List[str]:
+        """
+        Pop the specified variable from this collection as a list of strings.
+
+        If the keyword "default" is passed then that value, a list of strings, is returns if the specified variable
+        does not exist in this collection.
+        """
         if "default" in kwargs and name not in self:
             return kwargs["default"]
         values = self[name]
@@ -97,6 +127,16 @@ class MakeDict(object):
         return values
 
     def pop_value(self, name: str, **kwargs: Optional[Union[str, bool]]) -> Optional[str]:
+        """
+        Pop the specified variable from this collection as a single string.
+
+        If the keyword "default" is passed then that value, a string, is returns if the specified variable does not
+        exist in this collection.
+
+        By default the variable is expected to contain a single value.  However, if the keyword "combine" is passed
+        with a value of `True` then values are combined into a single string using a single space as the combining
+        seperator.
+        """
         if "default" in kwargs and name not in self:
             assert kwargs["default"] is None or isinstance(kwargs["default"], str)
             return kwargs["default"]
@@ -110,4 +150,5 @@ class MakeDict(object):
         return value
 
     def set(self, name: str, values: List[str]) -> None:
+        """Set the specified variable to the specified value."""
         self._variables[name] = values
