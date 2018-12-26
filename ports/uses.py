ee9202b65ff8db97a561738649a47aa17f81b3bf
David Naylor <naylor.b.david@gmail.com>
2018-12-26 11:09:26 +0200

    uses: replace doubel with single quotes

--- ports/uses.py
+++ ports/uses.py
@@ -1,7 +1,7 @@
 from typing import Dict, Iterable, List, Tuple
 from .core import MakeDict, Uses
 
-__all__ = ["Gnome", "PkgConfig", "ShebangFix"]
+__all__ = ['Gnome', 'PkgConfig', 'ShebangFix']
 
 
 def create_uses(name: str) -> type:
@@ -12,44 +12,44 @@ def create_uses(name: str) -> type:
     return UsesClass
 
 
-PkgConfig = create_uses("pkgconfig")
+PkgConfig = create_uses('pkgconfig')
 
 
-@Uses.register("gnome")
+@Uses.register('gnome')
 class Gnome(Uses):
     def __init__(self) -> None:
-        super(Gnome, self).__init__("gnome")
+        super(Gnome, self).__init__('gnome')
         self.components: List[str] = []
 
     def generate(self) -> Iterable[Tuple[str, Iterable[str]]]:
         if self.components:
-            yield ("USE_GNOME", self.components)
+            yield ('USE_GNOME', self.components)
 
     def load(self, variables: MakeDict) -> None:
-        self.components = variables.pop("USE_GNOME", default=[])
+        self.components = variables.pop('USE_GNOME', default=[])
 
 
-@Uses.register("shebangfix")
+@Uses.register('shebangfix')
 class ShebangFix(Uses):
     def __init__(self) -> None:
-        super(ShebangFix, self).__init__("shebangfix")
+        super(ShebangFix, self).__init__('shebangfix')
         self.files: List[str] = []
         self.languages: Dict[str, Tuple[str, str]] = {}
 
     def generate(self) -> Iterable[Tuple[str, Iterable[str]]]:
         if self.files:
-            yield ("SHEBANG_FILES", self.files)
+            yield ('SHEBANG_FILES', self.files)
         if self.languages:
-            yield ("SHEBANG_LANG", sorted(self.languages))
+            yield ('SHEBANG_LANG', sorted(self.languages))
             for lang in sorted(self.languages):
                 old_cmd, new_cmd = self.languages[lang]
-                yield ("%s_OLD_CMD" % lang, (old_cmd,))
-                yield ("%s_CMD" % lang, (new_cmd,))
+                yield ('%s_OLD_CMD' % lang, (old_cmd,))
+                yield ('%s_CMD' % lang, (new_cmd,))
 
     def load(self, variables: MakeDict) -> None:
-        self.files = variables.pop("SHEBANG_FILES", default=[])
-        for lang in variables.pop("SHEBANG_LANG", default=[]):
-            old_cmd = variables.pop_value("%s_OLD_CMD" % lang)
-            new_cmd = variables.pop_value("%s_CMD" % lang)
+        self.files = variables.pop('SHEBANG_FILES', default=[])
+        for lang in variables.pop('SHEBANG_LANG', default=[]):
+            old_cmd = variables.pop_value('%s_OLD_CMD' % lang)
+            new_cmd = variables.pop_value('%s_CMD' % lang)
             assert old_cmd is not None and new_cmd is not None
             self.languages[lang] = (old_cmd, new_cmd)
